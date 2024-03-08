bdc376ce9377bff24daba5ad7403e3979322b28d
Andreas Wolf <dev@a-w.io>
2024-03-08 12:31:50 +0100

    bugfix: Resolve best matching autoload.php file

--- fractor/bin/fractor.php
+++ fractor/bin/fractor.php
@@ -5,7 +5,24 @@ use a9f\Fractor\DependencyInjection\ContainerBuilder;
 use a9f\Fractor\FractorApplication;
 use Symfony\Component\Console\Input\ArgvInput;
 
-include __DIR__ . '/../vendor/autoload.php';
+$autoloadFile = (static function (): ?string {
+    $candidates = [
+        getcwd() . '/vendor/autoload.php',
+        __DIR__ . '/../../../autoload.php',
+        __DIR__ . '/../vendor/autoload.php',
+    ];
+    foreach ($candidates as $candidate) {
+        if (file_exists($candidate)) {
+            return $candidate;
+        }
+    }
+    return null;
+})();
+if ($autoloadFile === null) {
+    echo "Could not find autoload.php file";
+    exit(1);
+}
+include $autoloadFile;
 
 $configFile = ConfigResolver::resolveConfigsFromInput(new ArgvInput());
 
