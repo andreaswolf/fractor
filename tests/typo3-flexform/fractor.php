b7024c8774f70e2da96ace274641d953192f09ae
Andreas Wolf <dev@a-w.io>
2024-03-09 17:23:47 +0100

    task: Add end-to-end test

---
+++ tests/typo3-flexform/fractor.php
@@ -0,0 +1,19 @@
+<?php
+
+use a9f\Fractor\Configuration\FractorConfig;
+use a9f\Fractor\Fractor\DummyRule;
+use a9f\FractorXml\XmlFileProcessor;
+use a9f\Typo3Fractor\Rules\FlexForm\AddRenderTypeToFlexFormFractor;
+
+return static function (FractorConfig $config) {
+    $config->import(__DIR__ . '/../vendor/a9f/fractor-xml/config/fractor.php');
+
+    $config->setPaths([
+        __DIR__ . '/output/',
+    ]);
+
+    $config->withFileProcessor(XmlFileProcessor::class);
+
+    $config->withRule(AddRenderTypeToFlexFormFractor::class);
+    $config->withRule(DummyRule::class);
+};
