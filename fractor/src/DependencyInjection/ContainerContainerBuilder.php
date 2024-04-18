525b125cb4dcfaccb6377a57b12ad69e2b4a82ce
Sebastian Schreiber <me@schreibersebastian.de>
2024-04-18 13:10:21 +0200

    [TASK] Check for file existence

--- fractor/src/DependencyInjection/ContainerContainerBuilder.php
+++ fractor/src/DependencyInjection/ContainerContainerBuilder.php
@@ -3,7 +3,9 @@
 namespace a9f\Fractor\DependencyInjection;
 
 use a9f\Fractor\DependencyInjection\CompilerPass\CommandsCompilerPass;
+use a9f\FractorExtensionInstaller\Generated\InstalledPackages;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
@@ -14,13 +16,16 @@ class ContainerContainerBuilder
      */
     public function createDependencyInjectionContainer(array $additionalConfigFiles = []): ContainerInterface
     {
-        $containerBuilder = new \Symfony\Component\DependencyInjection\ContainerBuilder();
+        $containerBuilder = new ContainerBuilder();
         $this->loadFile($containerBuilder, __DIR__ . '/../../config/application.php');
         $this->importExtensionConfigurations($containerBuilder);
 
         $containerBuilder->addCompilerPass(new CommandsCompilerPass());
 
         foreach ($additionalConfigFiles as $additionalConfigFile) {
+            if (!file_exists($additionalConfigFile)) {
+                continue;
+            }
             $this->loadFile($containerBuilder, $additionalConfigFile);
         }
 
@@ -30,19 +35,19 @@ class ContainerContainerBuilder
     }
 
 
-    private function loadFile(\Symfony\Component\DependencyInjection\ContainerBuilder $containerBuilder, string $pathToFile): void
+    private function loadFile(ContainerBuilder $containerBuilder, string $pathToFile): void
     {
         $fileLoader = new PhpFileLoader($containerBuilder, new FileLocator(dirname($pathToFile)));
         $fileLoader->load($pathToFile);
     }
 
-    private function importExtensionConfigurations(\Symfony\Component\DependencyInjection\ContainerBuilder $containerBuilder): void
+    private function importExtensionConfigurations(ContainerBuilder $containerBuilder): void
     {
         if (!class_exists('a9f\\FractorExtensionInstaller\\Generated\\InstalledPackages')) {
             return;
         }
 
-        foreach (\a9f\FractorExtensionInstaller\Generated\InstalledPackages::PACKAGES as $package) {
+        foreach (InstalledPackages::PACKAGES as $package) {
             $filePath = $package['path'] . '/config/application.php';
 
             if (file_exists($filePath)) {
