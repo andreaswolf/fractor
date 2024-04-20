bef35f0ee50a3dd04a826b886066a79200c837be
Andreas Wolf <dev@a-w.io>
2024-04-20 14:02:53 +0200

    [TASK] Add Rector to all packages

---
+++ .build/rector.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+    ->withPhpSets(php82: true);
