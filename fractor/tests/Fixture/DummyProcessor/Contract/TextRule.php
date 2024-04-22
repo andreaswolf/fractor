f0eac5851e68ffacb4d9e52feca19ea96cc74d34
Sebastian Schreiber <me@schreibersebastian.de>
2024-04-22 10:40:06 +0200

    [TASK] Rename Helper to Fixture in tests folder

--- fractor/tests/Fixture/DummyProcessor/Contract/TextRule.php
+++ fractor/tests/Fixture/DummyProcessor/Contract/TextRule.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace a9f\Fractor\Tests\Helper\Contract;
+namespace a9f\Fractor\Tests\Fixture\DummyProcessor\Contract;
 
 use a9f\Fractor\Application\Contract\FractorRule;
 use a9f\Fractor\Application\ValueObject\File;
