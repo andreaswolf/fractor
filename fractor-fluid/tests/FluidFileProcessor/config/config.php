dfb8758a71e41374cb48d1b55b711b4c587ac0e1
Sebastian Schreiber <me@schreibersebastian.de>
2024-05-10 13:55:22 +0200

    [FEATURE] Add fractor-fluid package
    Resolves: #58

---
+++ fractor-fluid/tests/FluidFileProcessor/config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+use a9f\FractorFluid\Tests\Fixtures\DummyFluidFractorRule;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(__DIR__ . '/../../../config/application.php');
+
+    $services = $containerConfigurator->services();
+    $services->defaults()
+        ->autowire()
+        ->autoconfigure();
+
+    $services->set(DummyFluidFractorRule::class);
+};
