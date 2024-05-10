b49639d3e3db121ab9761a4134f066fa5a591ee2
Sebastian Schreiber <me@schreibersebastian.de>
2024-05-10 12:32:14 +0200

    [FEATURE] Out applied rules in console

---
+++ fractor/src/Reporting/FractorsChangelogResolver.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace a9f\Fractor\Reporting;
+
+use a9f\Fractor\Application\ValueObject\AppliedRule;
+
+final readonly class FractorsChangelogResolver
+{
+    public function __construct(
+        private ChangelogExtractor $changelogExtractor
+    ) {
+    }
+
+    /**
+     * @param AppliedRule[] $appliedRules
+     * @return array<class-string, string|null>
+     */
+    public function resolveIncludingMissing(array $appliedRules): array
+    {
+        $fractorClassesToChangelogUrls = [];
+        foreach ($appliedRules as $appliedRule) {
+            $fractorClass = $appliedRule->getFractorRule();
+            $changelogUrl = $this->changelogExtractor->extractChangelogFromRule($fractorClass);
+            $fractorClassesToChangelogUrls[$fractorClass] = $changelogUrl;
+        }
+
+        return $fractorClassesToChangelogUrls;
+    }
+}
---
+++ fractor/tests/Reporting/FractorsChangelogResolver/FractorsChangelogResolverTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace a9f\Fractor\Tests\Reporting\FractorsChangelogResolver;
+
+use a9f\Fractor\Application\ValueObject\AppliedRule;
+use a9f\Fractor\Reporting\ChangelogExtractor;
+use a9f\Fractor\Reporting\FractorsChangelogResolver;
+use a9f\Fractor\Tests\Fixture\DummyProcessor\Rules\ReplaceXXXTextRule;
+use PHPUnit\Framework\TestCase;
+
+final class FractorsChangelogResolverTest extends TestCase
+{
+    private FractorsChangelogResolver $subject;
+
+    protected function setUp(): void
+    {
+        $this->subject = new FractorsChangelogResolver(new ChangelogExtractor());
+    }
+
+    public function test(): void
+    {
+        // Arrange
+        $appliedRules = [
+            AppliedRule::fromRule(new ReplaceXXXTextRule())
+        ];
+
+        $expected = [
+            'a9f\Fractor\Tests\Fixture\DummyProcessor\Rules\ReplaceXXXTextRule' => 'https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog-11.html'
+        ];
+
+        // Act & Assert
+        self::assertSame($expected, $this->subject->resolveIncludingMissing($appliedRules));
+    }
+}
