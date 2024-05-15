589f606ec6e020af65fb2989c9bae75f44f4743e
Sebastian Schreiber <me@schreibersebastian.de>
2024-05-15 09:51:09 +0200

    [FEATURE] Add TranslationFileFractor for Yaml
    Relates: #60

---
+++ typo3-fractor/tests/Rules/TYPO3v10/Yaml/TranslationFileFractor/TranslationFileFractorTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rules\TYPO3v10\Yaml\TranslationFileFractor;
+
+use a9f\Fractor\Testing\PHPUnit\AbstractFractorTestCase;
+use Iterator;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class TranslationFileFractorTest extends AbstractFractorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixtures', '*.yaml');
+    }
+
+    public function provideConfigFilePath(): ?string
+    {
+        return __DIR__ . '/config/fractor.php';
+    }
+
+    protected function additionalConfigurationFiles(): array
+    {
+        return [__DIR__ . '/config/config.php'];
+    }
+}
