b918dc9afe72d95b95765149ccee3f2e7a2e9db2
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2024-06-19 00:14:01 +0200

    [FEATURE] Add MigrateTypeNoneColsToSizeFlexFormFractor

---
+++ packages/typo3-fractor/rules-tests/TYPO3v12/FlexForm/MigrateTypeNoneColsToSizeFlexFormFractor/MigrateTypeNoneColsToSizeFlexFormFractorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace a9f\Typo3Fractor\Tests\TYPO3v12\FlexForm\MigrateTypeNoneColsToSizeFlexFormFractor;
+
+use a9f\Fractor\Testing\PHPUnit\AbstractFractorTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class MigrateTypeNoneColsToSizeFlexFormFractorTest extends AbstractFractorTestCase
+{
+    #[DataProvider('provideData')]
+    public function test(string $filePath): void
+    {
+        $this->doTestFile($filePath);
+    }
+
+    public static function provideData(): \Iterator
+    {
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixtures', '*.xml');
+    }
+
+    public function provideConfigFilePath(): ?string
+    {
+        return __DIR__ . '/config/fractor.php';
+    }
+}
