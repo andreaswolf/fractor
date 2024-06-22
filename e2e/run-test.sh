1caa2acb35a018651375cca626383627722230eb
Andreas Wolf <dev@a-w.io>
2024-06-22 09:46:32 +0200

    [TASK] Add e2e test for TypoScript Fractor
    Also extends the e2e test script to run multiple tests and to compare
    the CLI output of Fractor

--- e2e/run-test.sh
+++ e2e/run-test.sh
@@ -10,15 +10,42 @@ cd $TESTS_BASE_DIR
 rm -r composer.lock vendor || true
 composer install
 
-TEST_DIR=typo3-yaml
+for TEST_DIR in typo3-yaml typo3-typoscript
+do
+    set +x
+    echo
+    echo "############################################################"
+    echo "#"
+    echo "# Running test in $TEST_DIR/"
+    echo "#"
+    echo "############################################################"
+    echo
+    set -x
 
-cd $TEST_DIR
+    # remove output from a previous run, if any
+    [[ -f $TEST_DIR/output.txt ]] && rm $TEST_DIR/output.txt
+    [[ -d $TEST_DIR/result/ ]] && rm -rf $TEST_DIR/result/
+    # copy over our fixture to the path that Fractor will run in
+    cp -r $TEST_DIR/fixtures/ $TEST_DIR/result/
 
-[[ -d ./output/ ]] && rm -rf ./output/
-cp -r fixtures/ output/
+    ./vendor/bin/fractor process -c $TESTS_BASE_DIR/$TEST_DIR/fractor.php > $TEST_DIR/output.txt
 
-cd $TESTS_BASE_DIR
-./vendor/bin/fractor process --quiet -c $TESTS_BASE_DIR/$TEST_DIR/fractor.php
+    set +x
+    echo
+    echo "############################################################"
+    echo "# Comparing Fractor result against expected result"
+    echo
+    set -x
 
-# TODO remove -b once we keep the output format when re-writing the file
-diff -rub $TEST_DIR/expected-output/ $TEST_DIR/output/
+    # TODO remove -b once we keep the output format when re-writing the file
+    diff -rub --color=auto $TEST_DIR/expected-result/ $TEST_DIR/result/
+
+    set +x
+    echo
+    echo "############################################################"
+    echo "# Comparing Fractor CLI output against expected output"
+    echo
+    set -x
+
+    diff -u --color=auto $TEST_DIR/expected-output.txt $TEST_DIR/output.txt
+done
