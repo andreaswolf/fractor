b5c40c502e5411bad49d4b0a1e61ddb46a12a8af
Andreas Wolf <dev@a-w.io>
2024-06-22 10:51:40 +0200

    [TASK] Port RemoveWorkspaceModeOptionsFractor from ssch/typo-rector 1.x

---
+++ packages/typo3-fractor/rules/TYPO3v12/TypoScript/RemoveWorkspaceModeOptionsFractor.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace a9f\Typo3Fractor\TYPO3v12\TypoScript;
+
+use a9f\FractorTypoScript\AbstractTypoScriptFractor;
+use a9f\FractorTypoScript\TypoScriptStatementsIterator;
+use Helmich\TypoScriptParser\Parser\AST\Operator\Assignment;
+use Helmich\TypoScriptParser\Parser\AST\Statement;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @changelog https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-98437-WorkspaceTSConfigSwapModeAndChangeStageModeRemoved.html
+ */
+final class RemoveWorkspaceModeOptionsFractor extends AbstractTypoScriptFractor
+{
+    public function refactor(Statement $statement): null|Statement|int
+    {
+        if (! $statement instanceof Assignment) {
+            return null;
+        }
+
+        if ($statement->object->absoluteName !== 'options.workspaces.swapMode'
+            && $statement->object->absoluteName !== 'options.workspaces.changeStageMode') {
+            return null;
+        }
+
+        return TypoScriptStatementsIterator::REMOVE_NODE;
+    }
+
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition(
+            'Remove TSConfig options.workspaces.swapMode and options.workspaces.changeStageMode',
+            [
+                new CodeSample(
+                    <<<'CODE_SAMPLE'
+options.workspaces.swapMode = any
+options.workspaces.changeStageMode = any
+CODE_SAMPLE
+                    ,
+                    <<<'CODE_SAMPLE'
+-
+CODE_SAMPLE
+                ),
+
+            ]
+        );
+    }
+}
