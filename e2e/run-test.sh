6b44f8db9821d3712c0625c0b3977711e9ad7fb6
Andreas Wolf <dev@a-w.io>
2024-07-13 12:24:14 +0200

    Include path in diff output
    Resolves: #195

--- e2e/run-test.sh
+++ e2e/run-test.sh
@@ -28,7 +28,8 @@ do
     # copy over our fixture to the path that Fractor will run in
     cp -r $TEST_DIR/fixtures/ $TEST_DIR/result/
 
-    ./vendor/bin/fractor process -c $TESTS_BASE_DIR/$TEST_DIR/fractor.php > $TEST_DIR/output.txt
+    ./vendor/bin/fractor process -c $TESTS_BASE_DIR/$TEST_DIR/fractor.php \
+        | sed -e "s#$TESTS_BASE_DIR#[e2e dir]#g" - > $TEST_DIR/output.txt
 
     set +x
     echo
