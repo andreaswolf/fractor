daea047b8d18e5d9bfdd4ea8d99cd410d7e38638
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-05-14 15:42:20 +0200

    [FEATURE] Add RemoveModWebLayoutDefLangBindingFractor
    Fixes: https://github.com/sabbelasichon/typo3-rector/issues/4548

--- packages/typo3-fractor/config/typo3-14.php
+++ packages/typo3-fractor/config/typo3-14.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use a9f\Typo3Fractor\TYPO3v14\TypoScript\RemoveExposeNonexistentUserInForgotPasswordDialogSettingInFeLoginFractor;
+use a9f\Typo3Fractor\TYPO3v14\TypoScript\RemoveModWebLayoutDefLangBindingFractor;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -12,4 +13,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire();
 
     $services->set(RemoveExposeNonexistentUserInForgotPasswordDialogSettingInFeLoginFractor::class);
+    $services->set(RemoveModWebLayoutDefLangBindingFractor::class);
 };
