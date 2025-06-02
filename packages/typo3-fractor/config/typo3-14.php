3ef6402aa47250fe10dc41e3de48d0b7fef48822
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-06-02 23:32:29 +0200

    [FEATURE] Add ChangeLogoutHandlingInFeLoginFractor (#295)
    Resolves: https://github.com/sabbelasichon/typo3-rector/issues/4508

--- packages/typo3-fractor/config/typo3-14.php
+++ packages/typo3-fractor/config/typo3-14.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use a9f\Typo3Fractor\TYPO3v14\Fluid\ChangeLogoutHandlingInFeLoginFractor;
 use a9f\Typo3Fractor\TYPO3v14\TypoScript\RemoveExposeNonexistentUserInForgotPasswordDialogSettingInFeLoginFractor;
 use a9f\Typo3Fractor\TYPO3v14\TypoScript\RemoveModWebLayoutDefLangBindingFractor;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -14,4 +15,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services->set(RemoveExposeNonexistentUserInForgotPasswordDialogSettingInFeLoginFractor::class);
     $services->set(RemoveModWebLayoutDefLangBindingFractor::class);
+    $services->set(ChangeLogoutHandlingInFeLoginFractor::class);
 };
