b70572aa698265a44b6ba2a1b1ae692916083f86
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2025-07-29 21:37:48 +0200

    [TASK] Remove MigrateBooleanAndNullAttributeValuesToNativeTypesFractor again
    Resolves: #312

--- packages/typo3-fractor/config/typo3-13.php
+++ packages/typo3-fractor/config/typo3-13.php
@@ -9,5 +9,4 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $services->defaults()
         ->autoconfigure()
         ->autowire();
-    //$services->set(\a9f\Typo3Fractor\TYPO3v13\Fluid\MigrateBooleanAndNullAttributeValuesToNativeTypesFractor::class); Do not use rule as it doesn't respect aria attributes which still have to be "true"
 };
