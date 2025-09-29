23da5846583e51caf017fd3cfa5dd7cb1aa5268e
cs <cs@amedick-sommer.de>
2025-09-29 22:00:21 +0200

    [FEATURE] Add XmlFormatConfiguration and integrate into XmlFileProcessor
    ✨ Introduced XmlFormatConfiguration for managing allowed file extensions.
    🔧 Updated XmlFileProcessor to utilize XmlFormatConfiguration for file extension validation.

--- packages/fractor-xml/src/Configuration/XmlProcessorOption.php
+++ packages/fractor-xml/src/Configuration/XmlProcessorOption.php
@@ -9,4 +9,6 @@ final class XmlProcessorOption
     public const INDENT_SIZE = 'xml-processor-indent-size';
 
     public const INDENT_CHARACTER = 'xml-processor-indent-character';
+
+    public const ALLOWED_FILE_EXTENSIONS = 'xml-processor-allowed-file-extensions';
 }
