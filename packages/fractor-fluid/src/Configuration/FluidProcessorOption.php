b1ea724b4e70d5bcfcb4ab9b62f6607a78db0b53
cs <cs@amedick-sommer.de>
2025-09-29 21:58:57 +0200

    [FEATURE] Introduce FluidFormatConfiguration and update FluidFileProcessor
    🌟 Add FluidFormatConfiguration class to manage allowed file extensions.
    🔧 Update FluidFileProcessor to utilize FluidFormatConfiguration for file extension handling.

---
+++ packages/fractor-fluid/src/Configuration/FluidProcessorOption.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace a9f\FractorFluid\Configuration;
+
+final class FluidProcessorOption
+{
+    public const ALLOWED_FILE_EXTENSIONS = 'fluid-processor-allowed-file-extensions';
+}
