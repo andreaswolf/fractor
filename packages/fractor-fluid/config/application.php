878e06a0e205c4ac4bf706dd87d014c7c72d012f
cs <cs@amedick-sommer.de>
2025-09-29 21:58:57 +0200

    [FEATURE] Introduce FluidFormatConfiguration and update FluidFileProcessor
    🌟 Add FluidFormatConfiguration class to manage allowed file extensions.
    🔧 Update FluidFileProcessor to utilize FluidFormatConfiguration for file extension handling.

--- packages/fractor-fluid/config/application.php
+++ packages/fractor-fluid/config/application.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use a9f\FractorFluid\Contract\FluidFractorRule;
 use a9f\FractorFluid\FluidFileProcessor;
+use a9f\FractorFluid\ValueObject\FluidFormatConfiguration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
 
 return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
@@ -16,8 +18,12 @@ return static function (ContainerConfigurator $containerConfigurator, ContainerB
 
     $services->load('a9f\\FractorFluid\\', __DIR__ . '/../src/');
 
+    $services->set('fractor.fluid_processor.format_configuration', FluidFormatConfiguration::class)
+        ->factory([null, 'createFromParameterBag']);
+
     $services->set(FluidFileProcessor::class)
-        ->arg('$rules', tagged_iterator('fractor.fluid_rule'));
+        ->arg('$rules', tagged_iterator('fractor.fluid_rule'))
+        ->arg('$fluidFormatConfiguration', service('fractor.fluid_processor.format_configuration'));
 
     $containerBuilder->registerForAutoconfiguration(FluidFractorRule::class)->addTag('fractor.fluid_rule');
 };
